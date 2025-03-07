3e5e39cc462dfafb0769370ed200b82907730e75
Haydn <haydn.morris@ivy-solutions.de>
2025-03-07 15:57:27 +0100

    add phone and email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -240,10 +240,11 @@ export default function Home() {
         </section>
       </main>
       <footer className="border-t py-6 md:py-0 px-container">
-        <div className="flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
-          <p className="text-sm text-muted-foreground">
+        <div className="flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row text-sm text-muted-foreground">
+          <p>
             &copy; {new Date().getFullYear()} Haydn Morris. All rights reserved.
           </p>
+          <p>+31 6 40 46 66 45 | hello@haydnjm.com</p>
           {/* <nav className="flex gap-4 sm:gap-6">
             <Link
               href="#"
