596466acba09b31d84d3627e97927dfb04ecc56f
Haydn <haydn.morris@ivy-solutions.de>
2025-03-10 11:36:55 +0100

    fix case studies link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,12 +89,12 @@ export default function Home() {
         </section>
 
         <section
-          id="projects"
+          id="case-studies"
           className="p-container py-12 md:py-24 lg:py-32 bg-muted/50"
         >
           <div className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-4 text-center">
             <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-5xl">
-              Projects
+              Case Studies
             </h2>
             <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
               Each project represents my commitment to quality, attention to
